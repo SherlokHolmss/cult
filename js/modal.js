3762a1a938d0dbdf4125fe3f2c1a1308587a93e6
Davud <david.poninka@gmail.com>
2024-10-12 11:40:29 +0300

    push

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,15 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    modalContent: document.querySelector(".modal-content"),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", () => toggleModal(500));
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleModal(width) {
+    refs.modal.classList.toggle("is-active");
+    if (width) {
+      refs.modalContent.style.maxWidth = `${width}px`;
+    }
   }
 })();
